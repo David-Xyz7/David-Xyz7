fb32822861e462108afaa59a91eec19d0fa55377
MR.D7 <81473463+David-Xyz7@users.noreply.github.com>
2021-09-16 22:55:33 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
-const target = {
-  clicked: 0,
-  currentFollowers: 90,
-  btn: document.querySelector("a.btn"),
-  fw: document.querySelector("span.followers")
-};
+/* Source Code :
+Dashboard by.  Olsi Odobashi Pro  - https://codepen.io/olsiodobashi/pen/dpvBgZ
 
-const follow = () => {
-  target.clicked += 1;
-  target.btn.innerHTML = 'Following <i class="fas fa-user-times"></i>';
-
-  if (target.clicked % 2 === 0) {
-    target.currentFollowers -= 1;
-    target.btn.innerHTML = 'Follow <i class="fas fa-user-plus"></i>';
-  }
-  else {
-    target.currentFollowers += 1;
-  }
-
-  target.fw.textContent = target.currentFollowers;
-  target.btn.classList.toggle("following");
-}
+Design Code is edited by. M-2010 : http://sample-mys2010.blogspot.co.id/2017/03/dashboard-menu.html
+*/
